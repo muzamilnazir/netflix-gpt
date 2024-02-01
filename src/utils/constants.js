ccf796e1d5a519419dad91625a4771ee9cf7a743
muzamil nazir <muzamilnazir11@gmail.com>
2024-02-01 09:40:08 +0530

    changed default logo

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const IMAGE_URL = "https://avatars.githubusercontent.com/u/76688722?v=4"
+export const IMAGE_URL = "https://images.ctfassets.net/y2ske730sjqp/5QQ9SVIdc1tmkqrtFnG9U1/de758bba0f65dcc1c6bc1f31f161003d/BrandAssets_Logos_02-NSymbol.jpg?w=940"
 export const LOGO_URL = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 export const OPTIONS = {
     method: 'GET',
